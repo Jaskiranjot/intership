e58a42e10ab191b4b6b35df25bfb36f6bf2f2555
JOY <67387240+Jaskiranjot@users.noreply.github.com>
2020-09-21 02:56:19 -0600

    navbar components appear on header

---
+++ profile/src/components/Navbar/MenuItems.js
@@ -0,0 +1,22 @@
+export const MenuItems = [
+    {
+        title: 'Profile',
+        url:'#',
+        cName: 'nav-Links'
+    },
+    {
+        title: 'Opportunity',
+        url:'#',
+        cName: 'nav-Links'
+    },
+    {
+        title: 'Resources',
+        url:'#',
+        cName: 'nav-Links'
+    },
+    {
+        title: 'Hours',
+        url:'#',
+        cName: 'nav-Links'
+    }
+]
