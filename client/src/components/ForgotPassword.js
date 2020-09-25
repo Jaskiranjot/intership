ef2a30cc358b86344f8045a6eb13ff707f2d0a05
Iryna <iryna.tar18@gmail.com>
2020-09-24 22:45:12 -0600

    initial sign in commit

--- client/src/components/ForgotPassword.js
+++ client/src/components/ForgotPassword.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+
 
 const ForgotPassword = () => {
-    return <div>Forgot password</div>
-};
+    const buttonText = { text: 'Send email'};
+    const labelText = { email: 'Email'};
+    return (
+        <div>
+            <div className="forgot-password">
+                <h2>Forgot password?</h2>
+                <p>Enter your email and we will send you a link to reset your password</p>
+                <div className="form-field">
+                    <label className="label" htmlFor="email">{labelText.email}</label>
+                    <input id='email' type='email' placeholder="example@gmail.com"/>
+                </div>
+                <div> 
+                    <button>{buttonText.text}</button>
+                </div> 
+                    <Link to='/change-password'>Change Password</Link>
+                </div>
+            </div>);
+            };
+    
 
 export default ForgotPassword;
--- client/src/components/Signin.js
+++ client/src/components/Signin.js
@@ -1,31 +1,33 @@
 import React from 'react';
-import Link from './Link';
+import {Link} from 'react-router-dom';
+
 
 const Signin = () => {
-        const buttonText = { text: 'Sign in'};
-        const labelText = { email: 'Email', password: 'Password'};
+    const buttonText = { text: 'Sign in'};
+    const labelText = { email: 'Email', password: 'Password'};
     return (
         <div className="sign-in">
-                <h1>Sign in</h1>
-                <div className="form-field">
-                    <label className="label" htmlFor="email">{labelText.email}</label>
-                    <input id='email' type='email' placeholder="example@gmail.com"/>
-                </div>
-                <div className="form-field">    
-                    <label className='label' htmlFor='password'>{labelText.password}</label>
-                    <input id='password' />
-                </div>
-                <div className='forgot-password'>
-                <Link href='/forgotpassword' className="forgorpassword">Forgot password?</Link>
-                </div>
-                <div className='create-account'>
-                    <Link href='create-account'>Create an account</Link>
-                </div>
-                <div> 
-                    <button>{buttonText.text}</button>
-                </div>  
+            <h2>Sign in</h2>
+        <div className="form-field">
+            <label className="label" htmlFor="email">{labelText.email}</label>
+            <input id='email' type='email' placeholder="example@gmail.com"/>
+        </div>
+        <div className="form-field">    
+            <label className='label' htmlFor='password'>{labelText.password}</label>
+            <input id='password' type='password'/>
+        </div>
+        <div className="link">
+            <Link to='/forgot-password' className="link" >Forgot password?</Link>
+        </div>
+        <div> 
+            <button>{buttonText.text}</button>
+        </div>  
+        <div id='create-account' className='link'>
+            <Link to='create-account' className='link'>Create an account</Link>
+        </div>
       </div>  
     )
 };
+
 
 export default Signin;
